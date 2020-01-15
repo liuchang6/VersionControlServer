19626d10489c7de8795716f24600396cb65a5838
liuchang6 <499926587@qq.com>
2020-01-15 23:38:08 +0800

    服务器数据库model，接口编写

--- version_control/models.py
+++ version_control/models.py
@@ -9,14 +9,19 @@ class ServerInfo(models.Model):
         (0, "已删除"),
         (1, "未删除"),
     )
+    status_choices = (
+        (0, "无法连接"),
+        (1, "连接正常"),
+    )
 
     name = models.CharField(max_length = 50,verbose_name = '名称')
     desc = models.CharField(max_length=2000, blank=True, null=True, verbose_name='描述')
     ip = models.GenericIPAddressField(verbose_name = 'IP地址')
     password = models.CharField(max_length=50, verbose_name='密码')
+    status = models.CharField(max_length=1, verbose_name='状态',choices=status_choices)
     create_time = models.DateTimeField('创建时间', auto_now_add=True)
     update_time = models.DateTimeField('更新时间', auto_now=True)
-    delete = models.CharField(max_length = 50,verbose_name = '是否删除：0 已删除；1 未删除',choices=delete_choices)
+    delete = models.CharField(max_length = 1,verbose_name = '是否删除',choices=delete_choices)
 
 
     class Meta():
